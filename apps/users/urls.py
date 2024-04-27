91eac0a8da13163f7ed319b1439ed271f46a6f02
DaigoDark19 <Diegomaluenga19@gmail.com>
2024-04-27 13:55:41 -0400

    creacion del test del creacion y registro del usuario

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from users.views import UserLoginAPIView, UserCreateAPIView
 
+app_name = 'users'
+
 urlpatterns = [
     path('register/', UserCreateAPIView.as_view(), name='register'),
     path('login/', UserLoginAPIView.as_view(), name='login'),
 ]
 
-app_name = 'users'
