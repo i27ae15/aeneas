0149c8e492c3817b810690c9f5fd0346160e0f61
DaigoDark19 <Diegomaluenga19@gmail.com>
2024-04-30 16:26:12 -0400

    creation of section post and its test

--- apps/task/test/view/section.py
+++ apps/task/test/view/section.py
@@ -6,45 +6,63 @@ from task.models.section import Section
 
 
 class SectionAPIViewTest(APITestCase):
-
-    def test_get_single_section(self):
-
-        user = CustomUser.objects.create_user(
+    # setup
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
             email='newuser@gmail.com',
             username='newuser',
             password='newpassword'
             )
-        section = Section.objects.create(
+        self.section = Section.objects.create(
+            user=self.user,
             name='section_new',
-            user=user
+            description='section_de_prueba'
         )
-        pk = section.pk
+        self.dataST = {
+            'user': self.user.id,
+            'name': 'section_new',
+            'description': 'section_de_prueba'
+        }
+        self.dataSF = {
+            'user': '9999999999999',
+            'name': 'section_new',
+            'description': 'section_de_prueba'
+        }
+
+    # GET
+    def test_get_single_section(self):
+
+        pk = self.section.pk
         url = reverse('task:section', kwargs={'pk': pk})
-        self.client.force_authenticate(user=user)
+        self.client.force_authenticate(user=self.user)
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['name'], section.name)
+        self.assertEqual(response.data['name'], self.section.name)
 
     def test_all_section(self):
 
-        user = CustomUser.objects.create_user(
-            email='newuser@gmail.com',
-            username='newuser',
-            password='newpassword'
-            )
-        self.client.force_authenticate(user=user)
+        self.client.force_authenticate(user=self.user)
         url = reverse('task:section')
         response = self.client.get(url)
         print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_noexiste_section(self):
-        user = CustomUser.objects.create_user(
-            email='newuser@gmail.com',
-            username='newuser',
-            password='newpassword'
-            )
-        self.client.force_authenticate(user=user)
+
+        self.client.force_authenticate(user=self.user)
         url = reverse('task:section', kwargs={'pk': 9999})
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    # POST
+    def test_created_201(self):
+        self.client.force_authenticate(user=self.user)
+        url = reverse('task:section')
+        response = self.client.post(url, self.dataST, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_bad_request_400(self):
+        self.client.force_authenticate(user=self.user)
+        url = reverse('task:section')
+        response = self.client.post(url, self.dataSF, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- apps/task/view/section.py
+++ apps/task/view/section.py
@@ -27,3 +27,10 @@ class SectionAPIView(APIView):
             queryset = Section.objects.all()
             serializer = SectionSerializer(queryset, many=True)
             return Response(serializer.data)
+
+    def post(self, request):
+        serializer = SectionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
